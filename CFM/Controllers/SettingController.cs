ce176d7742b05b259ae94413326c30f956baf972
CaoHuy <caohuy.cantho@gmail.com>
2024-03-06 09:12:48 +0700

    Setting not done

--- CFM/Controllers/SettingController.cs
+++ CFM/Controllers/SettingController.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using MyLibrary.DataAccess;
 using MyLibrary.Repository;
 
 namespace CFM.Controllers
@@ -19,5 +21,26 @@ namespace CFM.Controllers
             return Json(settingList);
         }
 
+        [HttpPost]
+        public IActionResult BankUpdate(string bank_id, string bank_number, string bank_template, string bank_content)
+        {
+            System.Console.WriteLine(bank_template);
+            UpdateSetting("bank_id", bank_id);
+            UpdateSetting("bank_number", bank_number);
+            UpdateSetting("bank_template", bank_template);
+            UpdateSetting("bank_content", bank_content);
+            return RedirectToAction("Index");
+        }
+        private void UpdateSetting(string key, string value)
+        {
+            Coffee_ManagementContext context = new Coffee_ManagementContext();
+            var setting = context.Settings.SingleOrDefault(s => s.Key == key);
+
+            if (setting != null)
+            {
+                setting.Value = value;
+                context.SaveChanges();
+            }
+        }
     }
 }
