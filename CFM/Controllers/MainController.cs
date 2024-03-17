49ff3793ea857b1b31732dd42105688374a58492
Winnnnnnnnnnn <thangle2003ss@gmail.com>
2024-03-17 21:16:09 +0700

    main

--- CFM/Controllers/MainController.cs
+++ CFM/Controllers/MainController.cs
@@ -5,17 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyLibrary.DataAccess;
 using MyLibrary.Repository;
+using MyMVC.Models.Authentication;
 
 namespace CFM.Controllers
 {
+    [Authentication]
     public class MainController : Controller
     {
-
+        private Coffee_ManagementContext dbContext = null;
         private readonly IProductRepository productRepository;
         private readonly ITableRepository tableRepository;
         public MainController()
         {
+            dbContext = new Coffee_ManagementContext();
             productRepository = new ProductRepository();
             tableRepository = new TableRepository();
         }
@@ -31,6 +35,23 @@ namespace CFM.Controllers
                 products = productRepository.GetProducts(),
                 tables = tableRepository.GetTables(),
             });
+        }
+        public object GetOrderByTableId(int id)
+        {
+            Order order = dbContext.Orders.FirstOrDefault(o => o.TableId == id && o.Status == 0);
+            if (order != null)
+            {
+                return Json(new
+                {
+                    order = order,
+                    details = order.GetDetail(),
+                    table = order.GetTable(),
+                });
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
     }
 }
